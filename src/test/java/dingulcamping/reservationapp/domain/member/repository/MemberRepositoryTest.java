c069d0be37be2f04a66e337d7fa5c4142445ee9d
hb912 <hb912@naver.com>
2023-06-09 21:28:52 +0900

    Test: MemberRepositoryTest 구현
    멤버를 저장하고 UserId로 select하는 로직 Test
    test의 무결성을 위해 beforeEach로 만약 save할 userId가 있는 row가 있다면 삭제해준다
    
    /**오류, 중요한부분
    beforeEach에서 delete를 한 후 바로 데이터 베이스에 반영되지않고 save를 했을 때 save의 우선순위가 높아 uniqueException이 터진다. 따라서 delete후 save를 한다면 꼭 em.flush를 해줄것

---
+++ src/test/java/dingulcamping/reservationapp/domain/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,72 @@
+package dingulcamping.reservationapp.domain.member.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import dingulcamping.reservationapp.domain.member.entity.Member;
+import dingulcamping.reservationapp.domain.member.entity.QMember;
+import dingulcamping.reservationapp.domain.member.entity.Role;
+import jakarta.persistence.EntityManager;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static dingulcamping.reservationapp.domain.member.entity.Role.USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before(){
+        Optional<Member> hb912 = memberRepository.findOneByUserId("hb912");
+        Optional<Member> hb915 = memberRepository.findOneByUserId("hb915");
+
+        if(hb912.isPresent()){
+            memberRepository.deleteByUserId(hb912.get().getUserId());
+            System.out.println("hb912 삭제");
+        }
+        if(hb915.isPresent()){
+            memberRepository.deleteByUserId(hb915.get().getUserId());
+            System.out.println("hb915 삭제");
+        }
+        em.flush();     //*주의 save의 우선순위가 더 높기 때문에 flush를 사용하지 않으면 delete가 반영되지 않는다.
+    }
+
+    /**
+     * test save, findById
+     */
+    @Test
+    @Rollback(value = false)
+    public void saveAndSelectById(){
+        Member memberA=new Member("hb912","memberA","12344456","ab@ab.com","010-5031-8478");
+        Member memberB=new Member("hb915","memberB","123444577","ab@abb.com","010-4444-8478");
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        Optional<Member> findMemberA = memberRepository.findOneByUserId(memberA.getUserId());
+        Optional<Member> findMemberB = memberRepository.findOneByUserId(memberB.getUserId());
+
+        assertThat(findMemberA.isPresent()).isTrue();
+        assertThat(findMemberA.get()).isEqualTo(memberA);
+        assertThat(findMemberB.isPresent()).isTrue();
+        assertThat(findMemberB.get()).isEqualTo(memberB);
+
+    }
+
+}
