8cd76cfd65536fff6018c25c73647abc57951250
hb912 <hb912@naver.com>
2023-06-12 16:16:57 +0900

    Feat: RoomRepository 생성
    Room class 생성자 & addImg 생성
    Enum RoomType 생성
    RoomRepository 생성

--- src/main/java/dingulcamping/reservationapp/domain/room/entity/Room.java
+++ src/main/java/dingulcamping/reservationapp/domain/room/entity/Room.java
@@ -6,27 +6,54 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
 public class Room extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique=true, nullable = false)
     private String name;
     private int price;
     private String content;
 
     @ElementCollection(fetch=FetchType.LAZY)
-    private List<String> imgSrc;
-    private String roomType;
+    private List<String> imgSrc=new ArrayList<>();
+
+    @Enumerated(value=EnumType.STRING)
+    private RoomType roomType;
+
     private String icon;
     private int maxPeople;
     private int minPeople;
 
     @Embedded
     private MapPosition mapPosition;
+
+    public Room(String name, int price, String content, List<String> imgSrc, RoomType roomType, String icon,
+                int maxPeople, int minPeople, MapPosition mapPosition) {
+        this.name = name;
+        this.price = price;
+        this.content = content;
+        this.imgSrc.addAll(imgSrc);
+        this.roomType = roomType;
+        this.icon = icon;
+        this.maxPeople = maxPeople;
+        this.minPeople = minPeople;
+        this.mapPosition = mapPosition;
+    }
+
+    public void addImg(String imgSrc){
+        this.imgSrc.add(imgSrc);
+    }
+
+    public void deleteImg(String imgSrc){
+        this.imgSrc.remove(imgSrc);
+    }
 }
---
+++ src/main/java/dingulcamping/reservationapp/domain/room/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package dingulcamping.reservationapp.domain.room.repository;
+
+import dingulcamping.reservationapp.domain.room.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
+    Optional<Room> findOneByName(String name);
+}
