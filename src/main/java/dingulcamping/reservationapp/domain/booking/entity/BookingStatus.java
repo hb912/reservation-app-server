d8a44517092f544180e4de8153f93a1fe223f92d
hb912 <hb912@naver.com>
2023-06-19 15:49:39 +0900

    Feat: bookingEntity 생성
    processDate는 ElementCollection으로 생성할때 자동으로 채워짐 ex) startDate:2023-0722, endDate:2023-0724 processDate: 2023-0722,2023-0723

---
+++ src/main/java/dingulcamping/reservationapp/domain/booking/entity/BookingStatus.java
@@ -0,0 +1,14 @@
+package dingulcamping.reservationapp.domain.booking.entity;
+
+public enum BookingStatus {
+    BOOKING_REQ("예약 요청"),BOOKING_CONFIRM("예약 확정"),BOOKING_EXP("예약 만료"),BOOKING_CANCEL("예약 취소");
+    final private String status;
+
+    BookingStatus(String status){
+        this.status=status;
+    }
+
+    String getStatus(){
+        return status;
+    }
+}
