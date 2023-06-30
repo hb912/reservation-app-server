0d94bd24d46449f3d19ee710af8e3a03387c1add
hb912 <hb912@naver.com>
2023-07-01 04:06:56 +0900

    feat: refreshToken 인증 로직 구현
    AccessToken이 만료되었을 때 refreshToken을 이용하여 AccessToken을 재발급 받는 로직 구현

--- src/main/java/dingulcamping/reservationapp/global/security/JwtFilter.java
+++ src/main/java/dingulcamping/reservationapp/global/security/JwtFilter.java
@@ -51,6 +51,20 @@ public class JwtFilter extends OncePerRequestFilter {
 //        //Token Expired 되었는지 여부
         if(JwtUtils.isExpired(token,secretKey)){
             log.error("Access Token이 만료되었습니다");
+            String refreshToken = getRefreshToken(request);
+            if(refreshToken.isEmpty()){
+                filterChain.doFilter(request,response);
+                return;
+            }
+            if(JwtUtils.isExpired(refreshToken,secretKey)||!checkRefreshToken(refreshToken,secretKey)){
+                log.error("RefreshToken이 만료되었습니다.");
+                filterChain.doFilter(request,response);
+                return;
+            }
+            memberId=JwtUtils.getMemberId(refreshToken,secretKey);
+            role=Role.valueOf(JwtUtils.getRole(refreshToken,secretKey));
+            String newAccessToken = JwtUtils.createJwt(memberId, role, secretKey, 30 * 60 * 1000l);
+            response.addHeader("authorization", "Bearer "+newAccessToken);
         }else{
             memberId = JwtUtils.getMemberId(token, secretKey);
             role=Role.valueOf(JwtUtils.getRole(token,secretKey));
@@ -63,3 +77,31 @@ public class JwtFilter extends OncePerRequestFilter {
         filterChain.doFilter(request,response);
 
     }
+
+    public Boolean checkRefreshToken(String token, String secretKey){
+        try{
+            Long memberId = JwtUtils.getMemberId(token, secretKey);
+            Long findMemberId = refreshTokenService.getMemberId(token);
+            return findMemberId==memberId;
+        }catch(ExpiredJwtException e){
+            return false;
+        }
+    }
+
+    private String getRefreshToken(HttpServletRequest request) {
+        try{
+            Cookie[] cookies = request.getCookies();
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("refreshToken")){
+                    String refreshToken=cookie.getValue();
+                    return refreshToken;
+                }
+            }
+            log.error("RefreshToken 없음");
+            return null;
+        }catch(NullPointerException e){
+            log.error("RefreshToken 없음.");
+            return null;
+        }
+    }
+}
