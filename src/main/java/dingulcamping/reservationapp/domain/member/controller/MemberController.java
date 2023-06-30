55726d2b571d14e9e2da66c91520dead39e92108
hb912 <hb912@naver.com>
2023-07-01 04:00:43 +0900

    feat: login Controller 구현

--- src/main/java/dingulcamping/reservationapp/domain/member/controller/MemberController.java
+++ src/main/java/dingulcamping/reservationapp/domain/member/controller/MemberController.java
@@ -1,28 +1,40 @@
 package dingulcamping.reservationapp.domain.member.controller;
 
+import dingulcamping.reservationapp.domain.member.dto.LoginDto;
 import dingulcamping.reservationapp.domain.member.dto.RegisterReqDto;
+import dingulcamping.reservationapp.domain.member.dto.TokenDto;
 import dingulcamping.reservationapp.domain.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/api/register")
+    @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterReqDto registerReqDto){
         log.info("회원가입 시작");
         memberService.register(registerReqDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody LoginDto loginDto, HttpServletResponse response){
+        TokenDto tokens = memberService.login(loginDto);
+        log.info("accessToken={}",tokens.getAccessToken());
+        String accessToken="Bearer "+tokens.getAccessToken();
+        response.addHeader("authorization", accessToken);
+        return ResponseEntity.ok("로그인이 완료되었습니다.");
     }
 }
