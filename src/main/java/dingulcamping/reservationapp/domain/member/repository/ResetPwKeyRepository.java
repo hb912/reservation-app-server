1496d90e47cae199a87e2a102f98dab8b1f68b6b
hb912 <hb912@naver.com>
2023-07-07 23:56:27 +0900

    feat: ResetPwKey Redis Entity 구현
    패스워드 변경을 위한 레디스 키를 저장할 엔티티 구현, repository 구현

---
+++ src/main/java/dingulcamping/reservationapp/domain/member/repository/ResetPwKeyRepository.java
@@ -0,0 +1,8 @@
+package dingulcamping.reservationapp.domain.member.repository;
+
+import dingulcamping.reservationapp.domain.member.entity.RefreshToken;
+import dingulcamping.reservationapp.domain.member.entity.ResetPwKey;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResetPwKeyRepository extends CrudRepository<ResetPwKey, String> {
+}
