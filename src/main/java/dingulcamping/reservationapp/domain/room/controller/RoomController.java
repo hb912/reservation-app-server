0b6c27630df3fb13e01644e8675db57a13656880
hb912 <hb912@naver.com>
2023-07-19 00:37:42 +0900

    feat: RoomId로 Room 조회 API 구현

---
+++ src/main/java/dingulcamping/reservationapp/domain/room/controller/RoomController.java
@@ -0,0 +1,31 @@
+package dingulcamping.reservationapp.domain.room.controller;
+
+import dingulcamping.reservationapp.domain.room.dto.RoomCreateDto;
+import dingulcamping.reservationapp.domain.room.dto.RoomDetailDto;
+import dingulcamping.reservationapp.domain.room.dto.RoomInfoDto;
+import dingulcamping.reservationapp.domain.room.entity.Room;
+import dingulcamping.reservationapp.domain.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/api/room")
+@Slf4j
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<RoomDetailDto> roomDetail(@PathVariable("roomId") Long roomId){
+        RoomDetailDto roomDetail = roomService.getRoomDetail(Long.valueOf(roomId));
+        return ResponseEntity.ok(roomDetail);
+    }
+
+}
