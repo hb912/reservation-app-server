5ec00ad795f8362307c9145c65ad0cd09031a7cc
hb912 <hb912@naver.com>
2023-07-25 20:17:26 +0900

    feat: AuthUtils 변경
    memberId로 발급된 authentication을 통해(authenticationtoken의 이름이 memberId) memberId로 받아오는 방식으로 변경
    
    기존의 방식으론 refreshtoken을 response로 받았을때 request의 token값이 바뀌지 않아 tokenExpired 에러가 계속 뜬다.

--- src/main/java/dingulcamping/reservationapp/global/security/AuthUtils.java
+++ src/main/java/dingulcamping/reservationapp/global/security/AuthUtils.java
@@ -5,38 +5,28 @@ import dingulcamping.reservationapp.domain.member.exception.MemberIsNotExistExce
 import dingulcamping.reservationapp.domain.member.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AuthUtils {
 
     private final MemberRepository memberRepository;
-    @Value("${jwt.secret}")
-    private String secretKey;
 
     @Transactional(readOnly = true)
-    public Member getMember(HttpServletRequest request) {
-        final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-        String token = authorization.split(" ")[1];
-        try {
-            Long memberId = JwtUtils.getMemberId(token, secretKey);
-            return memberRepository.findById(memberId).orElseThrow(MemberIsNotExistException::new);
-        } catch (Exception e) {
-            return null;
-        }
+    public Member getMember() {
+        Long memberId = getMemberId();
+        return memberRepository.findById(memberId).orElseThrow(MemberIsNotExistException::new);
     }
 
-    public Long getMemberId(HttpServletRequest request) {
-        final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-        String token = authorization.split(" ")[1];
-        try {
-            return JwtUtils.getMemberId(token, secretKey);
-        } catch (Exception e) {
-            return null;
-        }
+    public Long getMemberId() {
+        String memberId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return Long.valueOf(memberId);
     }
 }
