2faaa3810720d899ad693882515106319060b7d2
hb912 <hb912@naver.com>
2023-07-26 20:15:14 +0900

    update: findDisableRoomsByDate dto projection으로 변경
    QSimpleRoomDto projection으로 select 하도록 변경

--- src/main/java/dingulcamping/reservationapp/domain/booking/repository/BookingRepositoryCustom.java
+++ src/main/java/dingulcamping/reservationapp/domain/booking/repository/BookingRepositoryCustom.java
@@ -1,9 +1,8 @@
 package dingulcamping.reservationapp.domain.booking.repository;
 
 import dingulcamping.reservationapp.domain.booking.dto.BookingInfoDto;
+import dingulcamping.reservationapp.domain.room.dto.SimpleRoomDto;
 import dingulcamping.reservationapp.domain.booking.entity.Booking;
-import dingulcamping.reservationapp.domain.member.entity.Member;
-import dingulcamping.reservationapp.domain.room.entity.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,7 +12,7 @@ import java.util.List;
 public interface BookingRepositoryCustom {
     Page<BookingInfoDto> findAllByMemberId(Long memberId, Pageable pageable);
     List<Date> findDisableDatesByRoomId(Long roomId);
-    List<Room> findDisableRoomsByDate(List<Date> dates);
+    List<SimpleRoomDto> findDisableRoomsByDate(List<Date> dates);
     List<Booking> findRequests();
     List<Booking> findConfirms();
     List<Booking> findExistBooking(List<Date> processDates, Long roomId);
