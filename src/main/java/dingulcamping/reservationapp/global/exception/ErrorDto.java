88de1be6428e2527751bdc0fd85c50700ff48a7a
hb912 <hb912@naver.com>
2023-08-08 16:05:14 +0900

    feat: errorDto 작성

---
+++ src/main/java/dingulcamping/reservationapp/global/exception/ErrorDto.java
@@ -0,0 +1,13 @@
+package dingulcamping.reservationapp.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorDto {
+    private int status;
+    private String message;
+}
