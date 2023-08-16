9bc1a586a6ad588fca3854e992310e7bac403ff1
hb912 <hb912@naver.com>
2023-08-16 18:46:40 +0900

    update: reviewRepository의 findByBooking 수정
    findByBooking -> return Optional<Review> 에서 return Optional<ReviewInfoDto>로 수정

--- src/main/java/dingulcamping/reservationapp/domain/review/repository/ReviewRepositoryCustom.java
+++ src/main/java/dingulcamping/reservationapp/domain/review/repository/ReviewRepositoryCustom.java
@@ -1,5 +1,6 @@
 package dingulcamping.reservationapp.domain.review.repository;
 
+import dingulcamping.reservationapp.domain.booking.entity.Booking;
 import dingulcamping.reservationapp.domain.review.dto.ReviewInfoDto;
 import dingulcamping.reservationapp.domain.review.entity.Review;
 import dingulcamping.reservationapp.domain.room.entity.Room;
@@ -7,8 +8,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepositoryCustom {
     Page<ReviewInfoDto> findByRoom(Room room, Pageable pageable);
     List<Review> findByMemberId(Long memberId);
+    Optional<ReviewInfoDto> findByBooking(Booking booking);
 }
--- src/main/java/dingulcamping/reservationapp/domain/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/dingulcamping/reservationapp/domain/review/repository/ReviewRepositoryImpl.java
@@ -2,6 +2,7 @@ package dingulcamping.reservationapp.domain.review.repository;
 
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import dingulcamping.reservationapp.domain.booking.entity.Booking;
 import dingulcamping.reservationapp.domain.review.dto.QReviewInfoDto;
 import dingulcamping.reservationapp.domain.review.dto.ReviewInfoDto;
 import dingulcamping.reservationapp.domain.review.entity.Review;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 import static dingulcamping.reservationapp.domain.booking.entity.QBooking.booking;
 import static dingulcamping.reservationapp.domain.member.entity.QMember.member;
@@ -53,6 +55,30 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
                 .where(room.id.eq(roomDto.getId()));
 
         return PageableExecutionUtils.getPage(reviews, pageable, () -> countQuery.fetchOne());
+    }
+
+    @Override
+    public Optional<ReviewInfoDto> findByBooking(Booking bookingDto) {
+        return Optional.ofNullable(
+                queryFactory
+                        .select(new QReviewInfoDto(
+                                review.id.as("_id"),
+                                room.id.as("roomId"),
+                                room.name.as("roomName"),
+                                member.id.as("memberId"),
+                                member.name.as("memberName"),
+                                booking.id.as("bookingID"),
+                                review.title,
+                                review.content,
+                                review.grade
+                        ))
+                        .from(review)
+                        .innerJoin(review.booking, booking)
+                        .innerJoin(booking.room, room)
+                        .innerJoin(booking.member, member)
+                        .where(booking.id.eq(bookingDto.getId()))
+                        .fetchOne()
+        );
     }
 
     @Override
