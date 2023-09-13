10e8a44a7a1ca30f87105632dbf58ef047b55a4c
hb912 <hb912@naver.com>
2023-09-13 22:01:12 +0900

    feat: Kakao 인증코드를 통한 Token을 받아오는 로직 구현
    webClient를 통해 kakao api 요청으로 kakao token 받아옴(추후 사용자 정보 열람을 위해 사용)

---
+++ src/main/java/dingulcamping/reservationapp/domain/member/service/kakao/KakaoUtils.java
@@ -0,0 +1,36 @@
+package dingulcamping.reservationapp.domain.member.service.kakao;
+
+import dingulcamping.reservationapp.domain.member.dto.kakao.KakaoTokenDto;
+import dingulcamping.reservationapp.domain.member.dto.kakao.KakaoUserInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class KakaoUtils {
+
+    private final WebClient webClient;
+    private static final String TOKEN_URI = "https://kauth.kakao.com/oauth/token";
+    private static final String REDIRECT_URI = "http://localhost:8080/api/oauth";
+    private static final String GRANT_TYPE = "authorization_code";
+    @Value("${kakao.key}")
+    private String CLIENT_ID;
+
+    public KakaoTokenDto getToken(String code){
+        String uri = TOKEN_URI + "?grant_type=" + GRANT_TYPE + "&client_id=" + CLIENT_ID+ "&redirect_uri=" + REDIRECT_URI + "&code=" + code;
+        System.out.println(uri);
+
+        return webClient.post()
+                .uri(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(KakaoTokenDto.class)
+                .blockFirst();
+    }
+
+}
