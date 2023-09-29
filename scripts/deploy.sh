69660c1d5cde3a50b334971e2b2878678e6c2415
hb912 <hb912@naver.com>
2023-09-29 23:16:06 +0900

    HOTFIX : dockerfile, deploy.sh 위치 변경

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
+
+# 기존 이미지 삭제
+sudo docker rmi chaehongkim/dingul_camping:latest
+
+# 도커허브 이미지 pull
+sudo docker pull chaehongkim/dingul_camping:latest
+
+# 도커 run
+docker run -d -p 8080:8080 --env-file=env_list.txt --name app chaehongkim/dingul_camping:latest
+
+# 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
+docker rmi -f $(docker images -f "dangling=true" -q) || true
